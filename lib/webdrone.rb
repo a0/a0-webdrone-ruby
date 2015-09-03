c755df4d9ef9d5568823f0e474ca6cac369338e9
Aldrin Martoq <aldrin@martoq.cl>
2015-09-03 19:01:24 -0300

    Create and quit a browser using a block.

--- lib/webdrone.rb
+++ lib/webdrone.rb
@@ -5,5 +5,14 @@ require 'selenium-webdriver'
 module Webdrone
   def self.new(*args)
     a0 = Webdrone::Browser.new *args
+    if block_given?
+      begin
+        yield a0
+      ensure
+        a0.quit
+      end    
+    else
+      a0
+    end
   end
 end
--- spec/webdrone_spec.rb
+++ spec/webdrone_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe Webdrone do
+  it 'can create a browser with a block' do
+    Webdrone.new do |a0|
+      puts 'OK'
+    end
+  end
+
   it 'can create a browser' do
     a0 = Webdrone.new
     a0.quit
