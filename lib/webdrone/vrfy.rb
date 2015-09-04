dcf7503c96d5b02dfb5a1ae9b3984b98db822c11
Aldrin Martoq <aldrin@martoq.cl>
2015-09-04 03:29:19 -0300

    Simple verify test.

---
+++ lib/webdrone/vrfy.rb
@@ -0,0 +1,55 @@
+module Webdrone
+  class Browser
+    def vrfy
+      @vrfy ||= Vrfy.new self
+    end
+  end
+
+  class Vrfy
+    attr_accessor :a0
+
+    def initialize(a0)
+      @a0 = a0
+    end
+
+    def id(id, attr: nil, eq: nil, contains: nil)
+      vrfy @a0.find.id(id), attr: attr, eq: eq, contains: contains
+    end
+
+    def link(text, n: 1, visible: true, attr: nil, eq: nil, contains: nil)
+      vrfy @a0.find.link(text, n: n, visible: visible), attr: attr, eq: eq, contains: contains
+    end
+
+    def button(text, n: 1, visible: true, attr: nil, eq: nil, contains: nil)
+      vrfy @a0.find.button(text, n: n, visible: visible), attr: attr, eq: eq, contains: contains
+    end
+
+    def on(text, n: 1, visible: true, attr: nil, eq: nil, contains: nil)
+      vrfy @a0.find.on(text, n: n, visible: visible), attr: attr, eq: eq, contains: contains
+    end
+
+    def xpath(text, n: 1, visible: true, attr: nil, eq: nil, contains: nil)
+      vrfy @a0.find.xpath(text, n: n, visible: visible), attr: attr, eq: eq, contains: contains
+    end
+    
+    def vrfy(item, attr: nil, eq: nil, contains: nil)
+      if attr != nil
+        r = item.attribute(attr) == eq if eq != nil
+        r = item.attribute(attr).include? contains if contains != nil
+      elsif eq != nil
+        r = item.text == eq
+      elsif contains != nil
+        r = item.text.include? contains
+      end
+      if r == false
+        targ = "eq: [#{eq}]" if eq
+        targ = "contains: [#{contains}]" if contains
+        if attr != nil
+          raise Exception.new "ERROR: Attribute [#{attr}] value [#{item.attribute(attr)}] does not comply #{targ}"
+        else
+          raise Exception.new "ERROR: Value [#{item.text}] does not comply #{targ}"
+        end
+      end
+    end
+  end
+end
