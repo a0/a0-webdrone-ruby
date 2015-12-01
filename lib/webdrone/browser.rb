09eb301a5e5c320650a7a1a833aa0f2241a94aef
Aldrin Martoq <aldrin@martoq.cl>
2015-11-30 23:12:36 -0300

    New options when create a webdrone: enforcing timeout, maximize, create_outdir now is true by default.

--- lib/webdrone/browser.rb
+++ lib/webdrone/browser.rb
@@ -2,14 +2,14 @@ module Webdrone
   class Browser
     attr_accessor :driver
 
-    def initialize(browser: 'chrome', create_outdir: false, outdir: nil)
+    def initialize(browser: 'chrome', create_outdir: true, outdir: nil, timeout:, maximize: true)
       if create_outdir or outdir
         outdir ||= "webdrone_output/#{Time.new.strftime('%Y%m%d_%H%M')}"
         self.conf.outdir = outdir
       end
       if outdir != nil and browser.to_sym == :chrome
         prefs = { download: { prompt_for_download: false, default_directory: outdir } }
-        @driver = Selenium::WebDriver.for browser.to_sym, prefs: prefs
+        @driver = Selenium::WebDriver.for browser.to_sym, prefs: prefs, args: ['--disable-popup-blocking']
       elsif outdir != nil and browser.to_sym == :firefox
         profile = Selenium::WebDriver::Firefox::Profile.new
         profile['startup.homepage_welcome_url.additional'] = 'about:blank'
@@ -21,6 +21,12 @@ module Webdrone
       else
         @driver = Selenium::WebDriver.for browser.to_sym
       end
+      self.conf.timeout = timeout if timeout
+      self.maximize if maximize
+    end
+
+    def maximize
+      @driver.manage.window.maximize
     end
 
     def quit
