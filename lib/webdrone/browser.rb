24bac7b7fc096e5b3554a720a40cccfea004fd2a
jrovegno <javier.rovegno@gmail.com>
2016-02-09 12:41:06 -0300

    Fix download.dir path for windows

--- lib/webdrone/browser.rb
+++ lib/webdrone/browser.rb
@@ -37,7 +37,14 @@ module Webdrone
         @driver = Selenium::WebDriver.for browser.to_sym, prefs: chrome_prefs, args: ['--disable-popup-blocking']
       elsif outdir != nil and browser.to_sym == :firefox
         firefox_profile = Browser.firefox_profile if firefox_profile == nil
-        firefox_profile['browser.download.dir'] = outdir
+        case RbConfig::CONFIG['host_os'].downcase
+          when /mingw|mswin/
+            # Windows
+            downdir = outdir.gsub("/", "\\")
+          else
+            downdir = outdir
+        end
+        firefox_profile['browser.download.dir'] = downdir
         @driver = Selenium::WebDriver.for browser.to_sym, profile: firefox_profile
       else
         @driver = Selenium::WebDriver.for browser.to_sym
