ef7655b0b2c4ec80dcc0debf473148b9d83d4c74
Aldrin Martoq <aldrin@martoq.cl>
2016-03-24 02:46:28 -0300

    New HTML suppor for extracting html source from an element.

---
+++ lib/webdrone/html.rb
@@ -0,0 +1,36 @@
+module Webdrone
+  class Browser
+    def html
+      @html ||= Html.new self
+    end
+  end
+
+  class Html
+    attr_accessor :a0
+
+    def initialize(a0)
+      @a0 = a0
+    end
+
+    def find_html(text, n: 1, all: false, visible: true)
+      item = @a0.find.send __callee__, text, n: n, all: all, visible: visible
+      if item.is_a? Array
+        item.collect { |x| x.attribute 'innerHTML'}
+      else
+        item.attribute 'innerHTML'
+      end
+    rescue => exception
+      Webdrone.report_error(@a0, exception, Kernel.caller_locations)
+    end
+
+    alias_method :id,     :find_html
+    alias_method :css,    :find_html
+    alias_method :link,   :find_html
+    alias_method :button, :find_html
+    alias_method :on,     :find_html
+    alias_method :option, :find_html
+    alias_method :xpath,  :find_html
+
+    protected :find_html
+  end
+end
