f4b4e3c0e90d1ee31de20da514594cfdc53f3f28
Aldrin Martoq <aldrin@martoq.cl>
2016-03-26 00:09:06 -0300

    Rewriting mark.

--- lib/webdrone/mark.rb
+++ lib/webdrone/mark.rb
@@ -12,69 +12,43 @@ module Webdrone
       @a0 = a0
     end
 
-    def id(text, color: 'red', n: 1, all: false, visible: true)
-      flash @a0.find.id(text, n: n, all: all, visible: visible), color: color
-    rescue => exception
-      Webdrone.report_error(@a0, exception, Kernel.caller_locations)
-    end
-
-    def css(text, color: 'red', n: 1, all: false, visible: true)
-      flash @a0.find.css(text, n: n, all: all, visible: visible), color: color
-    rescue => exception
-      Webdrone.report_error(@a0, exception, Kernel.caller_locations)
-    end
-
-    def link(text, color: 'red', n: 1, all: false, visible: true)
-      flash @a0.find.link(text, n: n, all: all, visible: visible), color: color
-    rescue => exception
-      Webdrone.report_error(@a0, exception, Kernel.caller_locations)
-    end
-
-    def button(text, color: 'red', n: 1, all: false, visible: true)
-      flash @a0.find.button(text, n: n, all: all, visible: visible), color: color
-    rescue => exception
-      Webdrone.report_error(@a0, exception, Kernel.caller_locations)
-    end
-
-    def on(text, color: 'red', n: 1, all: false, visible: true)
-      flash @a0.find.on(text, n: n, all: all, visible: visible), color: color
-    rescue => exception
-      Webdrone.report_error(@a0, exception, Kernel.caller_locations)
-    end
-
-    def option(text, color: 'red', n: 1, all: false, visible: true)
-      flash @a0.find.option(text, n: n, all: all, visible: visible), color: color
-    rescue => exception
-      Webdrone.report_error(@a0, exception, Kernel.caller_locations)
-    end
-
-    def xpath(text, color: 'red', n: 1, all: false, visible: true)
-      flash @a0.find.xpath(text, n: n, all: all, visible: visible), color: color
-    rescue => exception
-      Webdrone.report_error(@a0, exception, Kernel.caller_locations)
-    end
-
-    def flash(item, color: 'red')
-      3.times do
-        border item, 'white'
-        sleep 0.05
-        border item, 'black'
-        sleep 0.05
-        border item, color
-        sleep 0.05
-      end
+    def mark(text, n: 1, all: false, visible: true, color: 'red', times: 3, sleep: 0.05)
+      item = @a0.find.send __callee__, text, n: n, all: all, visible: visible
+      mark_item item, color: color, times: times, sleep: sleep
       item
+    rescue => exception
+      Webdrone.report_error(@a0, exception, Kernel.caller_locations)
     end
 
-    protected
-      def border(item, color)
-        if item.is_a? Array
-          item.each do |item|
-            @a0.exec.script("arguments[0].style.border = '2px solid #{color}'", item)
-          end
-        else
+    alias_method :id,     :mark
+    alias_method :css,    :mark
+    alias_method :link,   :mark
+    alias_method :button, :mark
+    alias_method :on,     :mark
+    alias_method :option, :mark
+    alias_method :xpath,  :mark
+
+    def mark_item(item, color: 'red', times: 3, sleep: 0.05)
+      times.times do
+        mark_item_border item, 'white'
+        sleep sleep
+        mark_item_border item, 'black'
+        sleep sleep
+        mark_item_border item, color
+        sleep sleep
+      end
+    end
+
+    def mark_item_border(item, color)
+      if item.is_a? Array
+        item.each do |item|
           @a0.exec.script("arguments[0].style.border = '2px solid #{color}'", item)
         end
+      else
+        @a0.exec.script("arguments[0].style.border = '2px solid #{color}'", item)
       end
+    end
+
+    protected :mark, :mark_item, :mark_item_border
   end
 end
