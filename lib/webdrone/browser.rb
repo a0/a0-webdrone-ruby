319cac3b6fb559ef9a5b28a9e2c1895b4f17b8ae
Aldrin Martoq <aldrin@martoq.cl>
2016-06-05 00:06:38 -0400

    Bugfix: missing env.

--- lib/webdrone/browser.rb
+++ lib/webdrone/browser.rb
@@ -39,7 +39,7 @@ module Webdrone
       end
     end
 
-    def initialize(browser: 'firefox', create_outdir: true, outdir: nil, timeout:, developer: false, quit_at_exit: false, maximize: true, error: :raise_report, win_x: nil, win_y: nil, win_w: nil, win_h: nil, chrome_prefs: nil, firefox_profile: nil)
+    def initialize(browser: 'firefox', create_outdir: true, outdir: nil, timeout:, developer: false, quit_at_exit: false, maximize: true, error: :raise_report, win_x: nil, win_y: nil, win_w: nil, win_h: nil, use_env: true, chrome_prefs: nil, firefox_profile: nil)
       env_update(Kernel.binding) if use_env
       if create_outdir or outdir
         outdir ||= File.join("webdrone_output", Time.new.strftime('%Y%m%d_%H%M%S'))
